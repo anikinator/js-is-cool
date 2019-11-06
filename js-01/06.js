abbbbfcf96392407e9861a3dcce7879a3c3ab305
Constantine Anikin <kot@Constantines-MacBook-Pro.local>
2019-11-07 00:51:20 +0200

    task 6, 7

---
+++ js-01/06.js
@@ -0,0 +1,7 @@
+let count = 20
+
+for (i = 0; i <= 20; i++ ){
+    if (i % 2 == 0){
+        console.log(Math.pow(i,2))
+    }
+}
---
+++ js-01/07.js
@@ -0,0 +1,3 @@
+let username = prompt("enter user name")
+
+console.log("happy burthday to you, mr. " + username)
